2fb33748370f98e5063f57b9715aad20f99595da
divya.iyyani <divya.iyyani@somaiya.edu>
2018-04-10 22:35:13 +0530

    Fashion blog in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { UrlService } from './url.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UrlService]
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  constructor(private _urlService: UrlService) {
+
+  }
+
+  ngOnInit() {
+    this.title = this._urlService.callurl();
+}
 }
---
+++ src/app/url.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UrlService {
+
+    callurl()
+    {return 'Powered by Angular';
+  }
+  
+}
+
